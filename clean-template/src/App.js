989c416156d370d16358310b5c48e9f6e0307e36
Adrian <adrianpar@hotmail.com>
2023-07-01 17:59:29 +0200

    cleanTemplateReady

---
+++ clean-template/src/App.js
@@ -0,0 +1,19 @@
+// import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis id ad sit,
+        debitis sed praesentium repellendus cupiditate animi! Praesentium, vel.
+        Asperiores, nulla, quia est voluptate itaque ullam esse dignissimos
+        soluta neque numquam hic consequuntur voluptatem voluptatum a enim iste
+        iure exercitationem. Ipsa corporis voluptatum rerum accusantium impedit
+        libero ipsum exercitationem!
+      </p>
+    </div>
+  );
+}
+
+export default App;
