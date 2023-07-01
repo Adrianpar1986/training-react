461637a8a9ceba9f2411ae437b0d7ac51e79fb50
Adrian <adrianpar@hotmail.com>
2023-07-01 21:28:53 +0200

    cleanTemplateReady

--- clean-template/src/App.js
+++ clean-template/src/App.js
@@ -3,14 +3,14 @@
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello World!</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis id ad sit,
-        debitis sed praesentium repellendus cupiditate animi! Praesentium, vel.
-        Asperiores, nulla, quia est voluptate itaque ullam esse dignissimos
-        soluta neque numquam hic consequuntur voluptatem voluptatum a enim iste
-        iure exercitationem. Ipsa corporis voluptatum rerum accusantium impedit
-        libero ipsum exercitationem!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla,
+        repellendus mollitia modi, a inventore ad adipisci deleniti tempora
+        molestiae excepturi, quidem pariatur? Nam iusto atque id veritatis quae,
+        corporis tempore ipsam delectus? Vel inventore delectus, suscipit non
+        eaque earum enim quidem? Et fugiat repudiandae commodi, eum repellat
+        ullam explicabo exercitationem!
       </p>
     </div>
   );
