245c6327687361e060a9fe6b2155cae9b5b8e967
Adrian <adrianpar@hotmail.com>
2023-07-03 22:32:17 +0200

    añadiendo nuevos componentes

--- components/src/App.js
+++ components/src/App.js
@@ -1,11 +1,27 @@
-import Title from "./components/Title";
-import Paragraph from "./components/Paragraph";
 import "./App.css";
+import Welcome from "./components/Welcome";
 function App() {
+  let today = new Date(Date.now()).toLocaleDateString();
+
+  //la forma normal 
+  const veryWelcome = (today) => {
+    console.log(today);
+    let div = document.createElement("div");
+    let h1 = document.createElement("h1");
+    h1.innerHTML = `hoy es ${today}`;
+    div.appendChild(h1);
+    let prap = {
+      userName: "Adrian",
+      todayDate: today
+    }
+  };
+  //
+
   return (
     <div className="main">
-      <Title></Title>
-      <Paragraph></Paragraph>
+      <Welcome name="Adrian" today={today}></Welcome>
+      <Welcome name="Aitana" today={today}></Welcome>
+      <Welcome name="World" today={today}></Welcome>
     </div>
   );
 }
