d7a71f1cfe23ae111cf14483a0d9be07820bb674
Adrian <adrianpar@hotmail.com>
2023-07-03 19:51:20 +0200

    ejemplo minimo componente

--- components/src/App.js
+++ components/src/App.js
@@ -1,17 +1,11 @@
-// import './App.css';
-
+import Title from "./components/Title";
+import Paragraph from "./components/Paragraph";
+import "./App.css";
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla,
-        repellendus mollitia modi, a inventore ad adipisci deleniti tempora
-        molestiae excepturi, quidem pariatur? Nam iusto atque id veritatis quae,
-        corporis tempore ipsam delectus? Vel inventore delectus, suscipit non
-        eaque earum enim quidem? Et fugiat repudiandae commodi, eum repellat
-        ullam explicabo exercitationem!
-      </p>
+    <div className="main">
+      <Title></Title>
+      <Paragraph></Paragraph>
     </div>
   );
 }
---
+++ components/src/components/Paragraph.js
@@ -0,0 +1,13 @@
+import "./Paragraph.css";
+
+function Paragraph() {
+  return (
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla,
+      repellendus mollitia modi, a inventore ad adipisci deleniti tempora
+      molestiae excepturi,
+    </p>
+  );
+}
+
+export default Paragraph;
