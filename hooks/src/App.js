1fc2609bdc51dbd3c293114697ad67c0244ce7a5
Adrian <adrianpar@hotmail.com>
2023-07-12 22:01:24 +0200

    hooks

---
+++ hooks/src/App.js
@@ -0,0 +1,16 @@
+// import './App.css';
+import {useState} from "react"
+import Person from "./component/Person";
+
+function App() {
+  // const [count, setCount] = useState(0);
+  return (
+    <div>
+      <Person/>
+      {/* <p>Has clicado {count} veces</p>
+      <button onClick={()=>setCount(count +1)}>Pulsame</button> */}
+    </div>
+  );
+}
+
+export default App;
